b9d89aa8c766a4e8fddf77835440e3fac5c3d174
Akos Stegner <a.stegner@mito.hu>
2022-10-10 21:39:20 +0200

    Refact paginator

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -1,135 +1,92 @@
 <template>
-  <ul :class="mainClassName" v-if="pageCount > 1">
-    <li :class="{ 'is-disabled': page === 1 }">
-      <a href="#" @click.prevent="pageStep(-1)">
-        <slot name="prev">
-          <IconLeft class="icon paginator-left"></IconLeft>
-        </slot>
-      </a>
-    </li>
-    <li v-if="!isCompact" :class="{ 'is-disabled': page === 1 }">
-      <a href="#" type="button" @click.prevent="goTo(1)">
-        1
-      </a>
-    </li>
-    <li v-if="!isCompact" class="is-extended">
+  <div :class="mainClassName" v-if="pageCount > 1">
+    <button type="button" :disabled="page === 1" @click="pageStep(-1)">
+      <slot name="prev">
+        <IconLeft class="icon paginator-left"></IconLeft>
+      </slot>
+    </button>
+    <button type="button" v-if="!isCompact" :disabled="page === 1" @click="goTo(1)">1</button>
+    <div v-if="!isCompact" class="is-disabled">...</div>
+    <button
+      type="button"
+      v-for="n in range"
+      :key="n"
+      :class="{ 'is-active': n === page }"
+      :disabled="n === page"
+      @click="goTo(n)"
+    >
+      {{ n }}
+    </button>
+    <div v-if="!isCompact" class="is-disabled">
       <span>...</span>
-    </li>
-    <li v-for="n in range" :key="n" :class="{ 'is-active': n === page }">
-      <a href="#" @click.prevent="goTo(n)">
-        {{ n }}
-      </a>
-    </li>
-    <li v-if="!isCompact" class="is-extended">
-      <span>...</span>
-    </li>
-    <li v-if="!isCompact" :class="{ 'is-disabled': page === pageCount }">
-      <a href="#" @click.prevent="goTo(pageCount)">
-        {{ pageCount }}
-      </a>
-    </li>
-    <li :class="{ 'is-disabled': page === pageCount }">
-      <a href="#" @click.prevent="pageStep(1)">
-        <slot name="next">
-          <IconRight class="icon paginator-left"></IconRight>
-        </slot>
-      </a>
-    </li>
-  </ul>
+    </div>
+    <button type="button" v-if="!isCompact" :disabled="page === pageCount" @click="goTo(pageCount)">
+      {{ pageCount }}
+    </button>
+    <button type="button" :disabled="page === pageCount" @click.prevent="pageStep(1)">
+      <slot name="next">
+        <IconRight class="icon paginator-left"></IconRight>
+      </slot>
+    </button>
+  </div>
 </template>
-<script>
+<script setup>
+import { computed } from 'vue';
 import { useMainClass } from '../composables/useMainClass';
+import usePagination from '../composables/usePagination';
 import IconLeft from './icons/IconLeft.vue';
 import IconRight from './icons/IconRight.vue';
 
-export default {
-  name: 'Paginator',
-
-  components: {
-    IconLeft,
-    IconRight
+const props = defineProps({
+  page: {
+    type: Number,
+    required: false,
+    default: 0,
+    validator: (value) => {
+      return value >= 0;
+    },
   },
 
-  props: {
-    page: {
-      type: Number,
-      required: false,
-      default: 0,
-      validator: value => {
-        return value >= 0;
-      }
+  itemsPerPage: {
+    type: Number,
+    required: false,
+    default: 10,
+    validator: (value) => {
+      return value > 0;
     },
-
-    itemsPerPage: {
-      type: Number,
-      required: false,
-      default: 10,
-      validator: value => {
-        return value > 0;
-      }
-    },
-
-    totalItems: {
-      type: Number,
-      required: true,
-      validator: value => {
-        return value >= 0;
-      }
-    },
-
-    rangeLength: {
-      type: Number,
-      default: 3,
-      validator: value => {
-        return value >= 2;
-      }
-    },
-
-    isCompact: {
-      type: Boolean,
-      default: false
-    }
   },
 
-  setup() {
-    const mainClassName = useMainClass('paginator');
-
-    return {
-      mainClassName
-    }
+  totalItems: {
+    type: Number,
+    required: true,
+    validator: (value) => {
+      return value >= 0;
+    },
   },
 
-  computed: {
-    pageCount() {
-      return Math.ceil(this.totalItems / this.itemsPerPage);
+  rangeLength: {
+    type: Number,
+    default: 3,
+    validator: (value) => {
+      return value >= 2;
     },
-
-    range() {
-      const page = this.page - 1;
-      const range = Array.from({ length: this.pageCount }, (_, i) => i + 1);
-      const minus = Math.floor(this.rangeLength / 2);
-
-      let startIndex = Math.max(page - minus, 0);
-      if (startIndex + this.rangeLength >= this.pageCount) {
-        startIndex = Math.max(this.pageCount - this.rangeLength, 0);
-      }
-      return range.slice(startIndex, startIndex + this.rangeLength);
-    }
   },
 
-  methods: {
-    pageStep(v) {
-      const page = this.page + v;
-      if (this.validPage(page)) this.$emit('change', page);
-    },
+  isCompact: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-    goTo(page) {
-      if (this.validPage(page)) this.$emit('change', page);
-    },
+const emit = defineEmits(['change']);
 
-    validPage(page) {
-      return page <= this.pageCount && page > 0;
-    }
-  }
-};
+const mainClassName = useMainClass('paginator');
+
+const { page, pageCount, range, goTo, pageStep } = usePagination({
+  currentPage: computed(() => props.page),
+  totalItems: computed(() => props.totalItems),
+  itemsPerPage: props.itemsPerPage,
+  rangeLength: props.rangeLength,
+  update: (page) => emit('change', page),
+});
 </script>
