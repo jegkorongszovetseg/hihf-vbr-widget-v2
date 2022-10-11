bc087d86f9308dcb48639b3ae6c6d111cdfbd4e7
Akos Stegner <a.stegner@mito.hu>
2022-10-11 11:12:19 +0200

    Develop init

--- src/bundle.js
+++ src/bundle.js
@@ -1,4 +1,5 @@
 import { defineCustomElement } from 'vue';
 import Schedule from './components/widgets/Schedule.ce.vue';
+import './init.js';
 
 customElements.define('vbr-schedule', defineCustomElement(Schedule));
--- src/init.js
+++ src/init.js
@@ -1,5 +1,23 @@
 // alert('I am here');
-var initWidget = function (apiKey) {
-  window.__MJSZ_VBR_WIDGET__ = { apiKey };
-  alert('Key:' + apiKey);
-};
+// var initWidget = function (apiKey) {
+//   window.__MJSZ_VBR_WIDGET__ = { apiKey };
+//   alert('Key:' + apiKey);
+// };
+var SomeName = (function () {
+  var function1 = function (number) {
+    return number + 1;
+  };
+
+  var anotherFunction = function (number) {
+    return number + 2;
+  };
+
+  return {
+    function1: function (number) {
+      return function1(number);
+    },
+    function2: function (number) {
+      return anotherFunction(number);
+    },
+  };
+})();
