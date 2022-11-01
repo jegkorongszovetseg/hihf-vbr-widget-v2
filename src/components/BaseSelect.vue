66f7c1ca099ae782123844254fa455ea3de893f3
Akos Stegner <a.stegner@mito.hu>
2022-11-01 14:53:41 +0100

    Create Base Select

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from 'vue';
+import { useMainClass } from '../composables/useMainClass';
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: null,
+  },
+});
+
+const emits = defineEmits(['update:modelValue']);
+const baseInputClass = useMainClass('base-input');
+
+const value = computed({ get: () => props.modelValue, set: (value) => emits('update:modelValue', value) });
+</script>
+
+<template>
+  <select v-model="value" :class="[baseInputClass, 'is-select']">
+    <slot />
+  </select>
+</template>
--- src/components/widgets/extended/StatisticSelector.vue
+++ src/components/widgets/extended/StatisticSelector.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { computed } from 'vue';
 import { useI18n } from '../../../composables/useI18n';
+import { useMainClass } from '../../../composables/useMainClass';
 import { REPORT_TYPE_PLAYERS, REPORT_TYPE_TEAMS } from './statistics.internal';
+import BaseSelect from '../../BaseSelect.vue';
 
 const props = defineProps({
   seasons: {
@@ -94,43 +96,53 @@ const playerName = computed({
 const onStatTypeChange = (value) => {
   emit('onStatTypeChange', value);
 };
+
+const baseInputClass = useMainClass('base-input');
+const baseLabelClass = useMainClass('label');
 </script>
 
 <template>
   <div>
+    <button type="button" @click="onStatTypeChange(REPORT_TYPE_PLAYERS)">Players</button>
+    <button type="button" @click="onStatTypeChange(REPORT_TYPE_TEAMS)">Teams</button>
+  </div>
+  <div style="display: flex; flex-wrap: wrap; gap: 20px; margin: 20px 0">
     <div>
-      <button type="button" @click="onStatTypeChange(REPORT_TYPE_PLAYERS)">Players</button>
-      <button type="button" @click="onStatTypeChange(REPORT_TYPE_TEAMS)">Teams</button>
-    </div>
-    <div>
-      <label for="season">Szezon</label>
-      <select id="season" v-model="seasonSelect" class="mjsz-vbr-input">
+      <label for="season" :class="baseLabelClass">Szezon</label>
+      <BaseSelect id="season" v-model="seasonSelect">
         <option v-for="season in seasons" :key="season.championshipId" :value="season.championshipId">
           {{ season.seasonName }}
         </option>
-      </select>
+      </BaseSelect>
     </div>
     <div>
-      <label for="section">Szakasz</label>
-      <select id="section" v-model="sectionSelect">
+      <label for="section" :class="baseLabelClass">Szakasz</label>
+      <BaseSelect id="section" v-model="sectionSelect">
         <option v-for="sectionName in sections" :key="sectionName" :value="sectionName">{{ sectionName }}</option>
-      </select>
+      </BaseSelect>
     </div>
     <div>
-      <label for="report">Riport</label>
-      <select id="report" v-model="reportSelect">
+      <label for="report" :class="baseLabelClass">Riport</label>
+      <BaseSelect id="report" v-model="reportSelect">
         <option v-for="{ name, value } in reports" :key="value" :value="value">{{ name }}</option>
-      </select>
+      </BaseSelect>
     </div>
     <div>
-      <label for="teams">Teams</label>
-      <select id="teams" v-model="teamSelect" :disabled="reportType !== 'players'">
+      <label for="teams" :class="baseLabelClass">Teams</label>
+      <BaseSelect id="teams" v-model="teamSelect" :disabled="reportType !== 'players'">
         <option :value="null">{{ t('common.all') }}</option>
         <option v-for="{ teamId, teamName } in teams" :key="teamId" :value="teamId">{{ teamName }}</option>
-      </select>
+      </BaseSelect>
     </div>
     <div>
-      <input type="text" v-model="playerName" :disabled="reportType !== 'players'" />
+      <label for="player" :class="baseLabelClass">Szűrés névre</label>
+      <input
+        id="player"
+        type="text"
+        v-model="playerName"
+        :class="baseInputClass"
+        :disabled="reportType !== 'players'"
+      />
     </div>
   </div>
 </template>
