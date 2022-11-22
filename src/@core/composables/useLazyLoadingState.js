03bdacdcc53902afe678e321295b538044944d38
Akos Stegner <a.stegner@mito.hu>
2022-11-22 21:56:49 +0100

    Refact lazy load state

--- src/@core/composables/useLazyLoadingState.js
+++ src/@core/composables/useLazyLoadingState.js
@@ -1,30 +1,32 @@
-import { ref, unref, watch } from 'vue';
-import { refDebounced } from '@vueuse/shared';
+import { computed, ref, unref, watch } from 'vue';
+import { useTimeoutFn } from '@vueuse/shared';
 
 export function useLazyLoadingState(task = [], options = {}) {
   const { delay = 0 } = options;
 
-  const baseState = ref(false);
+  const isLoding = ref(false);
 
-  const getValue = (_task) => {
+  const state = computed(() => {
+    const _task = unref(task);
     if (Array.isArray(_task)) {
       return _task.map((item) => unref(item)).some(Boolean);
     }
     return _task;
-  };
+  });
 
-  const isLoading = refDebounced(baseState, delay);
+  const { start, stop } = useTimeoutFn(() => {
+    isLoding.value = true;
+  }, delay);
 
   watch(
-    task,
-    (task) => {
-      console.log('task:', task);
-      baseState.value = getValue(task);
+    state,
+    (_state) => {
+      if (_state) return start();
+      stop();
+      isLoding.value = false;
     },
     { immediate: true, deep: true }
   );
 
-  watch(isLoading, (il) => console.log('IS_LOADING:', il));
-
-  return isLoading;
+  return isLoding;
 }
