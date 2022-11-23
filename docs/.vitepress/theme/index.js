8d00b12e49067d7b5ba3befb520ae8706a923273
Akos Stegner <a.stegner@mito.hu>
2022-11-23 15:08:11 +0100

    Set for docs

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,13 @@
 import DefaultTheme from 'vitepress/theme';
-// import { createConfig } from '../../../dist/mjsz-vbr-core.es.js';
-// import Elements from '../../../dist/mjsz-vbr-elements.es.js';
-import { register } from '../../../dist/mjsz-vbr-bundle.es.js';
+import { createConfig } from '../../../dist/mjsz-vbr-bundle.es.js';
 
 export default {
   ...DefaultTheme,
   enhanceApp() {
     if (typeof window !== 'undefined') {
-      register();
-      // createConfig({
-      //   modules: [Elements],
-      //   apiKey: import.meta.env.VITE_VBR_API_KEY,
-      // });
+      createConfig({
+        apiKey: import.meta.env.VITE_VBR_API_KEY,
+      });
     }
   },
 };
