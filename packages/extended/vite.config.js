1892a7130464f34b1dd8f7d22996c1f73b5718aa
Akos Stegner <a.stegner@mito.hu>
2023-06-05 20:01:20 +0200

    Add banner for build Extended

--- packages/extended/vite.config.js
+++ packages/extended/vite.config.js
@@ -1,10 +1,20 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import banner from 'vite-plugin-banner';
+
+import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    banner({
+      content: `/**\n * name: ${pkg.name}\n * version: v${pkg.version} - ${new Date().toLocaleString(
+        'en-GB'
+      )}\n * (c) ${new Date().getFullYear()}\n * description: ${pkg.description}\n * author: ${pkg.author}\n */`,
+    }),
+  ],
 
   build: {
     lib: {
