c1dc1e852a8e0ee5a680c1f369cbd67d16d6ce66
Akos Stegner <a.stegner@mito.hu>
2023-06-07 10:56:52 +0200

    Add dedupe

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -9,6 +9,9 @@ import pkg from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    dedupe: ['@mjsz-vbr-elements/shared'],
+  },
   plugins: [
     vue(),
     banner({
