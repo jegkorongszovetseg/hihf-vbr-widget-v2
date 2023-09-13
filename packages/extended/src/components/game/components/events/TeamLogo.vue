43e1cf4359acf169f83c71ae9194cc8be1b3ec32
Akos Stegner <a.stegner@mito.hu>
2023-09-13 22:16:10 +0200

    Develop player numbers in events

--- packages/extended/src/components/game/components/events/TeamLogo.vue
+++ packages/extended/src/components/game/components/events/TeamLogo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { Image, FloatingPanel } from '@mjsz-vbr-elements/core/components';
 import { DEAFULT_LOGO_TEAM_A, DEAFULT_LOGO_TEAM_B } from '../../internal';
 
@@ -20,13 +20,16 @@ const props = defineProps({
   },
 });
 
+const tooltipContainer = ref(null);
+
 const defaultSrc = computed(() => (props.isHomeTeam ? DEAFULT_LOGO_TEAM_A : DEAFULT_LOGO_TEAM_B));
 </script>
 
 <template>
-  <FloatingPanel :offset="2" placement="top" theme="tooltip" :content="name" v-slot:default="{ setRef, show, hide }">
+  <FloatingPanel :offset="2" placement="top" theme="tooltip" :content="name" :append-to="tooltipContainer" v-slot:default="{ setRef, show, hide }">
     <span :ref="setRef" @mouseenter="show" @mouseleave="hide" @focus="show" @blur="hide" :tabindex="0">
       <Image :src="logo" :default-src="defaultSrc" />
     </span>
   </FloatingPanel>
+  <div ref="tooltipContainer" />
 </template>
