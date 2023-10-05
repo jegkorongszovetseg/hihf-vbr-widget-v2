295f03b3545807fd55258e2b36aa5294801a3a2d
Akos Stegner <a.stegner@mito.hu>
2023-10-05 09:26:52 +0200

    Delete unneeded components from gamcenter

--- packages/gamecenter/src/index.js
+++ packages/gamecenter/src/index.js
@@ -1,32 +1,13 @@
 import { defineCustomElement } from 'vue';
 
-import { Playoffs, Statistics, ScheduleLiga, Game, Championship } from './components';
+import Game from './components/game/Game.ce.vue';
 
-const StatisticsCE = defineCustomElement(Statistics);
-const ScheduleLigaCE = defineCustomElement(ScheduleLiga);
-const PlayoffsCE = defineCustomElement(Playoffs);
 const GameCE = defineCustomElement(Game);
-const ChampionshipCE = defineCustomElement(Championship);
 
-export {
-  Game,
-  Statistics,
-  ScheduleLiga,
-  Playoffs,
-  Championship,
-  StatisticsCE,
-  ScheduleLigaCE,
-  PlayoffsCE,
-  GameCE,
-  ChampionshipCE,
-};
+export { Game, GameCE };
 
 export function register() {
-  customElements.define('mjsz-vbr-statistics', StatisticsCE);
-  customElements.define('mjsz-vbr-schedule-liga', ScheduleLigaCE);
-  customElements.define('mjsz-vbr-playoffs', PlayoffsCE);
   customElements.define('mjsz-vbr-game', GameCE);
-  customElements.define('mjsz-vbr-championship', ChampionshipCE);
 }
 
 export default register;
