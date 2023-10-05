b106194d5c0a362a6106a7c653d4d2096cec5d11
Akos Stegner <a.stegner@mito.hu>
2023-10-05 12:18:08 +0200

    Delete unneeded components

--- packages/liga/src/components/championship/championship.internal.js
+++
@@ -1,7 +0,0 @@
-export const transformSections = (sections, state) => {
-  // state.sections = sections;
-  state.championships = sections;
-  state.selectedChampionshipId = sections?.[0]?.sectionId;
-  state.phaseId = sections?.[0]?.phases[0]?.phaseId;
-  // state.section = compose(prop('phaseName'), head)(state.sections);
-};
