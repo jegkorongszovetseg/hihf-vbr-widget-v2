e0ac5a61a5d5f0d5b2b0da16ac32a8d730d3c255
Akos Stegner <a.stegner@mito.hu>
2023-11-03 23:43:11 +0100

    Delete stringPadLeft function

--- packages/core/src/utils/datetime.js
+++ packages/core/src/utils/datetime.js
@@ -6,7 +6,6 @@ import _isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import _isBetween from 'dayjs/plugin/isBetween';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import 'dayjs/locale/hu';
-import { stringPadLeft } from './string.js';
 import { LOCALE_FOR_LANG } from '../constants.js';
 
 dayjs.extend(utc);
