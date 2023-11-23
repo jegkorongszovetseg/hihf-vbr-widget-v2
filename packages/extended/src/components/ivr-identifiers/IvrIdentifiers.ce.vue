866ac117cd765bab3698e6fae5fa2fb484d78349
Akos Stegner <a.stegner@mito.hu>
2023-11-23 14:26:37 +0100

    Develop IVR sections

--- packages/extended/src/components/ivr-identifiers/IvrIdentifiers.ce.vue
+++ packages/extended/src/components/ivr-identifiers/IvrIdentifiers.ce.vue
@@ -70,14 +70,18 @@ const props = defineProps({
             </section>
 
             <section class="is-mb-5">
-              <template v-for="section in sections" :key="section.sectionId">
-                <button
-                  :class="[useMainClass('tab-button'), { 'is-active': section.sectionId === sectionId }]"
-                  @click="onChangeSection(section.sectionId)"
-                >
-                  {{ section.sectionName }}
-                </button>
-              </template>
+              <label for="sections">{{ t('selection.sections') }}:</label>
+
+              <div id="sections" :class="[useMainClass('toggle-group')]">
+                <template v-for="section in sections" :key="section.sectionId">
+                  <button
+                    :class="{ 'is-active': section.sectionId === sectionId }"
+                    @click="onChangeSection(section.sectionId)"
+                  >
+                    {{ section.sectionName }}
+                  </button>
+                </template>
+              </div>
             </section>
 
             <section class="is-mb-5">
