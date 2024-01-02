6b0ec001ba77c429c33d041488cc334dcc51fc39
Akos Stegner <a.stegner@mito.hu>
2024-01-02 13:34:28 +0100

    feat: (schedule-liga) Sticky header top offset beállítási lehetőség

--- packages/core/src/composables/useScrollToGameDate.js
+++ packages/core/src/composables/useScrollToGameDate.js
@@ -6,9 +6,7 @@ import { format, isSameOrBefore } from '../utils/datetime';
 export const useScrollToGameDate = (options = {}) => {
   const { items = [], element, offset = 0 } = options;
 
-  const condition = (date) => {
-    return isSameOrBefore(date, 'day');
-  };
+  const condition = (date) => isSameOrBefore(date, 'day');
 
   const scrollToDatePosition = () => {
     stop();
@@ -17,7 +15,9 @@ export const useScrollToGameDate = (options = {}) => {
     const idDate = format(item.gameDate, 'YYYY-MM-DD');
     const dateElement = unrefElement(element).querySelector(`div[data-gamedate="${idDate}"]`);
     if (!dateElement) return;
-    window.scrollTo(0, (dateElement.getBoundingClientRect()?.top ?? 0) - unref(offset));
+    const topOffset = getComputedStyle(dateElement).getPropertyValue('--vbr-widget-sticky-top-offset') || 0;
+    const computedOffset = (dateElement.getBoundingClientRect()?.top ?? 0) - unref(offset) - parseFloat(topOffset);
+    window.scrollTo(0, computedOffset);
   };
 
   const { stop } = watchPausable(
