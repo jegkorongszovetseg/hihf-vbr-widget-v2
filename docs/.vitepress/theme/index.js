459bf892fe031f115990c3df159f60b644d6d4be
Akos Stegner <a.stegner@mito.hu>
2024-01-03 14:39:56 +0100

    Delete unneeded rows

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -12,14 +12,9 @@ export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     if (typeof window !== 'undefined') {
-      // createConfig({
-      //   modules: [MjszVbrElements, MjszVbrElementsExtended, MjszVbrElementsLiga, MjszVbrElementsGamecenter],
-      //   apiKey: 'dd8adf5fdb738b3741fa579b5ede5ce69b681f62',
-      // });
       app.use(MjszVbrElementsCore, {
         modules: [MjszVbrElements, MjszVbrElementsExtended, MjszVbrElementsLiga, MjszVbrElementsGamecenter],
         apiKey: import.meta.env.VITE_VBR_API_KEY,
-        // apiKey: 'dd8adf5fdb738b3741fa579b5ede5ce69b681f62',
       });
     }
 
