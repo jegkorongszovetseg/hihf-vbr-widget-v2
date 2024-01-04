23de10d0bae10c567b9c4dc56ef5ec66443d987f
Akos Stegner <a.stegner@mito.hu>
2024-01-04 20:58:07 +0100

    Switchable scroll to game date and fix game resolver

--- playground/src/pages/LigaSchedule.vue
+++ playground/src/pages/LigaSchedule.vue
@@ -6,6 +6,8 @@ import '../../../packages/shared/dist/css/typography.css';
 import '../../../packages/shared/dist/css/cards.css';
 import '../../../packages/shared/dist/css/grid.css';
 import '../../../packages/shared/dist/css/forms.css';
+
+const gameLinkResolver = '/GameCenter?gameId={gameId}'
 </script>
 
 <template>
@@ -15,7 +17,8 @@ import '../../../packages/shared/dist/css/forms.css';
       :locale="store.locale"
       championship-name="Erste Liga"
       timezone-selector
-      scroll-to-game-date="false"
+      :external-game-resolver="gameLinkResolver"
+      :scroll-to-game-date="true"
     />
   </div>
 </template>
