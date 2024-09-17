467e470fb57684dae2d08e313d93f3cb93dc9200
Sokamoka <akos.stegner@gmail.com>
2024-09-17 10:16:18 +0200

    Develop events

--- packages/gamecenter/src/components/game-timeline/components/events/Goal.vue
+++ packages/gamecenter/src/components/game-timeline/components/events/Goal.vue
@@ -3,6 +3,7 @@ import { computed, ref } from 'vue';
 import { reject, isEmpty } from 'ramda';
 import { FloatingPanel } from '@mjsz-vbr-elements/core/components';
 import IconHockeyPuck from '@mjsz-vbr-elements/shared/icons/IconHockeyPuck';
+import GameEventLayout from '../GameEventLayout.vue';
 import TeamLogo from './TeamLogo.vue';
 
 const props = defineProps({
@@ -25,85 +26,92 @@ const awayOnIce = computed(() => props.event.awayOnIce);
 </script>
 
 <template>
-  <div class="is-time-cell">{{ event.eventTime }}</div>
-  <div class="is-team-logo-cell">
-    <TeamLogo :name="event.team.longName" :logo="event.team.logo" :key="event.team.id" :is-home-team="isHomeTeam" />
-  </div>
-  <div class="is-icon-cell"><IconHockeyPuck class="is-goal-icon" /></div>
-  <div class="is-score">{{ event.score }}</div>
-  <div>
-    <span v-if="event.advantage" class="is-badge is-large">
-      {{ event.advantage }}
-    </span>
-    <span v-if="event.en" class="is-badge is-large"> EN </span>
-  </div>
-  <div>
-    <span v-if="event.ps || event.gws || event.gwg" class="is-badge is-invert is-large">
-      <template v-if="event.ps">PS</template>
-      <template v-if="event.gws">GWS</template>
-      <template v-if="event.gwg">GWG</template>
-    </span>
-  </div>
-  <div>
-    <dl>
-      <dt class="is-evented-person">
-        <span class="is-player-number">{{ event.jerseyNumber }}</span> {{ event.lastName }} {{ event.firstName }}
-      </dt>
-      <dt class="is-assists-list">
-        <template v-for="assist in assists" :key="assist">
-          <span
-            ><i>{{ assist.jerseyNumber }}</i> {{ assist.lastName }} {{ assist.firstName }}</span
-          >
-        </template>
-      </dt>
-      <dd v-if="!isEmpty(homeOnIce) || !isEmpty(awayOnIce)" class="is-poi-data">
-        <ul>
-          <template v-for="player in homeOnIce" :key="player.playerId">
-            <FloatingPanel
-              :offset="2"
-              placement="top"
-              theme="tooltip"
-              :content="`${player.lastName} ${player.firstName}`"
-              :append-to="tooltipContainer"
-              v-slot:default="{ setRef, events }"
-            >
-              <li
-                :ref="setRef"
-                :tabindex="0"
-                :aria-label="`${player.jerseyNumber} ${player.lastName} ${player.firstName}`"
-                v-on="events"
+  <GameEventLayout :timestamp="event.eventTime" :is-home-team="isHomeTeam">
+    <template #default>
+      <li class="is-score">{{ event.score }}</li>
+      <li>
+        <span v-if="event.advantage" class="is-badge is-large">
+          {{ event.advantage }}
+        </span>
+        <span v-if="event.en" class="is-badge is-large"> EN </span>
+      </li>
+      <li>
+        <span v-if="event.ps || event.gws || event.gwg" class="is-badge is-invert is-large">
+          <template v-if="event.ps">PS</template>
+          <template v-if="event.gws">GWS</template>
+          <template v-if="event.gwg">GWG</template>
+        </span>
+      </li>
+      <!-- <div>
+        <dl>
+          <dt class="is-evented-person">
+            <span class="is-player-number">{{ event.jerseyNumber }}</span> {{ event.lastName }} {{ event.firstName }}
+          </dt>
+          <dt class="is-assists-list">
+            <template v-for="assist in assists" :key="assist">
+              <span
+                ><i>{{ assist.jerseyNumber }}</i> {{ assist.lastName }} {{ assist.firstName }}</span
               >
-                {{ player.jerseyNumber }}
-              </li>
-            </FloatingPanel>
-          </template>
-        </ul>
+            </template>
+          </dt>
+          <dd v-if="!isEmpty(homeOnIce) || !isEmpty(awayOnIce)" class="is-poi-data">
+            <ul>
+              <template v-for="player in homeOnIce" :key="player.playerId">
+                <FloatingPanel
+                  :offset="2"
+                  placement="top"
+                  theme="tooltip"
+                  :content="`${player.lastName} ${player.firstName}`"
+                  :append-to="tooltipContainer"
+                  v-slot:default="{ setRef, events }"
+                >
+                  <li
+                    :ref="setRef"
+                    :tabindex="0"
+                    :aria-label="`${player.jerseyNumber} ${player.lastName} ${player.firstName}`"
+                    v-on="events"
+                  >
+                    {{ player.jerseyNumber }}
+                  </li>
+                </FloatingPanel>
+              </template>
+            </ul>
 
-        <span> / </span>
+            <span> / </span>
 
-        <ul>
-          <template v-for="player in awayOnIce" :key="player.playerId">
-            <FloatingPanel
-              :offset="2"
-              placement="top"
-              theme="tooltip"
-              :content="`${player.lastName} ${player.firstName}`"
-              :append-to="tooltipContainer"
-              v-slot:default="{ setRef, events }"
-            >
-              <li
-                :ref="setRef"
-                :tabindex="0"
-                :aria-label="`${player.jerseyNumber} ${player.lastName} ${player.firstName}`"
-                v-on="events"
-              >
-                {{ player.jerseyNumber }}
-              </li>
-            </FloatingPanel>
-          </template>
-        </ul>
-      </dd>
-    </dl>
-    <div ref="tooltipContainer" />
-  </div>
+            <ul>
+              <template v-for="player in awayOnIce" :key="player.playerId">
+                <FloatingPanel
+                  :offset="2"
+                  placement="top"
+                  theme="tooltip"
+                  :content="`${player.lastName} ${player.firstName}`"
+                  :append-to="tooltipContainer"
+                  v-slot:default="{ setRef, events }"
+                >
+                  <li
+                    :ref="setRef"
+                    :tabindex="0"
+                    :aria-label="`${player.jerseyNumber} ${player.lastName} ${player.firstName}`"
+                    v-on="events"
+                  >
+                    {{ player.jerseyNumber }}
+                  </li>
+                </FloatingPanel>
+              </template>
+            </ul>
+          </dd>
+        </dl>
+      </div> -->
+    </template>
+
+    <template #team-logo>
+      <TeamLogo :name="event.team.longName" :logo="event.team.logo" :key="event.team.id" :is-home-team="isHomeTeam" />
+    </template>
+
+    <template #event-type-icon>
+      <IconHockeyPuck />
+    </template>
+  </GameEventLayout>
+  <div ref="tooltipContainer" />
 </template>
--- packages/gamecenter/src/components/game-timeline/components/events/Goalies.vue
+++ packages/gamecenter/src/components/game-timeline/components/events/Goalies.vue
@@ -2,6 +2,7 @@
 import TeamLogo from './TeamLogo.vue';
 import IconArrowDown from '@mjsz-vbr-elements/shared/icons/IconArrowDown';
 import IconArrowUp from '@mjsz-vbr-elements/shared/icons/IconArrowUp';
+import GameEventLayout from '../GameEventLayout.vue';
 
 defineProps({
   event: {
@@ -17,19 +18,29 @@ defineProps({
 </script>
 
 <template>
-  <div class="is-time-cell">{{ event.eventTime }}</div>
-  <div class="is-team-logo-cell">
-    <TeamLogo :name="event.team.longName" :logo="event.team.logo" :key="event.team.id" :is-home-team="isHomeTeam" />
-  </div>
-  <div class="is-icon-cell">
-    <IconArrowUp v-if="event.gkDirection === 'CSERE'" width="24" height="24" class="is-goalie-in-icon" />
-    <IconArrowUp v-if="event.gkDirection === 'BE'" width="24" height="24" class="is-goalie-in-icon" />
-    <IconArrowDown v-else width="24" height="24" class="is-goalie-out-icon" />
-  </div>
-  <div class="is-goalie-direction">{{ event.gkDirection }}</div>
-  <div></div>
-  <div></div>
-  <div class="is-evented-person">
-    <span class="is-player-number">{{ event.jerseyNumber }}</span> {{ event.lastName }} {{ event.firstName }}
-  </div>
+  <GameEventLayout :timestamp="event.eventTime" :is-home-team="isHomeTeam">
+    <template #default>
+      <div class="is-icon-cell">
+        <IconArrowUp v-if="event.gkDirection === 'CSERE'" width="24" height="24" class="is-goalie-in-icon" />
+        <IconArrowUp v-if="event.gkDirection === 'BE'" width="24" height="24" class="is-goalie-in-icon" />
+        <IconArrowDown v-else width="24" height="24" class="is-goalie-out-icon" />
+      </div>
+      <div class="is-goalie-direction">{{ event.gkDirection }}</div>
+      <div></div>
+      <div></div>
+      <div class="is-evented-person">
+        <span class="is-player-number">{{ event.jerseyNumber }}</span> {{ event.lastName }} {{ event.firstName }}
+      </div>
+    </template>
+
+    <template #team-logo>
+      <TeamLogo :name="event.team.longName" :logo="event.team.logo" :key="event.team.id" :is-home-team="isHomeTeam" />
+    </template>
+
+    <template #event-type-icon>
+      <IconArrowUp v-if="event.gkDirection === 'CSERE'" width="24" height="24" class="is-goalie-in-icon" />
+      <IconArrowUp v-if="event.gkDirection === 'BE'" width="24" height="24" class="is-goalie-in-icon" />
+      <IconArrowDown v-else width="24" height="24" class="is-goalie-out-icon" />
+    </template>
+  </GameEventLayout>
 </template>
