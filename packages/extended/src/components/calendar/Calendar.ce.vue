0dbb2b748cd26b8a2d52f799ce261b6585c7c0a0
Sokamoka <akos.stegner@gmail.com>
2024-09-24 21:54:42 +0200

    fix: (extended) Scope css javítás

--- packages/extended/src/components/calendar/Calendar.ce.vue
+++ packages/extended/src/components/calendar/Calendar.ce.vue
@@ -156,7 +156,7 @@ const resolveExternalGameLink = (game) => externalGameLinkResolver(props.externa
   </I18NProvider>
 </template>
 
+<style src="@mjsz-vbr-elements/shared/css/typography.css"></style>
 <style src="@mjsz-vbr-elements/shared/css/common.css"></style>
 <style src="@mjsz-vbr-elements/shared/css/forms.css"></style>
 <style src="@mjsz-vbr-elements/shared/css/cards.css"></style>
-<style src="@mjsz-vbr-elements/shared/css/typography.css"></style>
