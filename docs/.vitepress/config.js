d1b0eb8ce8e4c65c4ae9b587a86e56c1a22ecd72
sokamoka <akos.stegner@gmail.com>
2025-01-14 14:16:22 +0100

    Update vitepress

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,9 @@
-import { process } from 'node:process';
 import { defineConfig, loadEnv } from 'vitepress';
 import pkg from '../../package.json';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line node/prefer-global/process
   const env = loadEnv(mode, process.cwd());
 
   return {
