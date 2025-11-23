cdffa4ef4aca812d4dd13402c2c6e72a2b2d780a
sokamoka <akos.stegner@gmail.com>
2025-11-23 21:41:48 +0100

    Develop Ad Placement tool

---
+++ packages/internal/src/components/ad-placement-tool/AdPlacement.ce.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { isBetween } from '@mjsz-vbr-elements/core/utils';
+import { computed } from 'vue';
+
+const banners = [
+  {
+    id: 0,
+    active: true,
+    link: 'xxx',
+    start: new Date(2025, 10, 23, 12, 0),
+    end: new Date(2025, 10, 25, 12, 0),
+  },
+];
+
+const activeAds = computed(() => {
+  return banners.filter(banner => banner.active && isBetween(new Date(), banner.start, banner.end));
+});
+
+const currentAd = computed(() => {
+  const random = Math.floor(Math.random() * activeAds.value.length);
+  return activeAds[random];
+});
+</script>
+
+<template>
+  <div>
+    <div class="desktop">
+      <a href="#">Desktop: {{ currentAd }}</a>
+    </div>
+    <div class="mobile">
+      <a href="#">Mobile</a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ packages/internal/vite.config.js
@@ -0,0 +1,53 @@
+import { resolve } from 'node:path';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import banner from 'vite-plugin-banner';
+import { compressConfig } from '../../compress.config';
+
+import pkg from './package.json';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    banner({
+      content: `/**\n * name: ${pkg.name}\n * version: v${pkg.version} - ${new Date().toLocaleString(
+        'en-GB',
+      )}\n * (c) ${new Date().getFullYear()}\n * description: ${pkg.description}\n * author: ${pkg.author}\n */`,
+    }),
+    ...compressConfig,
+  ],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'MjszVbrElementsInternal',
+      fileName: 'index',
+      formats: ['es', 'iife'],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: [
+        'vue',
+        '@mjsz-vbr-elements/core',
+        '@mjsz-vbr-elements/core/utils',
+        '@mjsz-vbr-elements/core/columns',
+        '@mjsz-vbr-elements/core/constants',
+        '@mjsz-vbr-elements/core/components',
+        '@mjsz-vbr-elements/core/composables',
+      ],
+      output: {
+        exports: 'named',
+        globals: {
+          'vue': 'Vue',
+          '@mjsz-vbr-elements/core': 'MjszVbrElementsCore',
+          '@mjsz-vbr-elements/core/utils': 'MjszVbrElementsCore',
+          '@mjsz-vbr-elements/core/columns': 'MjszVbrElementsCore',
+          '@mjsz-vbr-elements/core/constants': 'MjszVbrElementsCore',
+          '@mjsz-vbr-elements/core/components': 'MjszVbrElementsCore',
+          '@mjsz-vbr-elements/core/composables': 'MjszVbrElementsCore',
+        },
+      },
+    },
+  },
+});
