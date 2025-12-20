0c63f61ebbba7963f87ec40a3ce411ae5187f1d4
sokamoka <akos.stegner@gmail.com>
2025-12-20 21:44:19 +0100

    Develop md generation

--- docs/styles/component-variables.md
+++ docs/styles/component-variables.md
@@ -2,4 +2,19 @@
 outline: deep
 ---
 
+# Komponens CSS Változók
+
+Lehetséges az egyes összetevők egyedi beállítása is.
+Minden komponenshez tartozó CSS változók a `@layer components.variables` blokkban találhatók.
+
+::: tip A fájlodban ezeket a CSS változókat testreszabhatod
+
+```css
+* {
+  --mvw-avatar-border-color: light-dark(var(--mvw-color-primary-100), var(--mvw-color-primary-700));
+}
+```
+
+:::
+
 <!--@include: ../../packages/shared/dist/docs/component-variables.md -->
--- packages/shared/scripts/generate-component-variables.js
+++ packages/shared/scripts/generate-component-variables.js
@@ -26,25 +26,7 @@ function formatComponentName(filename) {
 function generateMarkdown() {
   const files = fs.readdirSync(componentsDir).filter(f => f.endsWith('.css'));
 
-  let markdown = `---
-outline: deep
----
-
-# Komponens CSS Változók
-
-Lehetséges az egyes összetevők egyedi beállítása is.
-Minden komponenshez tartozó CSS változók a \`@layer components.variables\` blokkban találhatók.
-
-::: tip A fájlodban ezeket a CSS változókat testreszabhatod
-
-\`\`\`css
-* {
-  --mvw-avatar-border-color: light-dark(var(--mvw-color-primary-100), var(--mvw-color-primary-700));
-}
-\`\`\`
-:::
-
-`;
+  let markdown = '';
 
   for (const file of files) {
     const filePath = path.join(componentsDir, file);
