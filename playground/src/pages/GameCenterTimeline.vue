0a21021bbbd2943605769cf4868652adeff8fe31
sokamoka <akos.stegner@gmail.com>
2025-12-20 10:16:07 +0100

    Test playground player resolver

--- playground/src/pages/GameCenterTimeline.vue
+++ playground/src/pages/GameCenterTimeline.vue
@@ -10,11 +10,13 @@ import { store } from '../store.js';
 // import '../../../packages/shared/dist/css/progress.css';
 
 useLigaCss();
+
+const resolver = '/LigaPlayer?playerId={player.playerId}&championshipId={championshipId}';
 </script>
 
 <template>
   <div class="p-5 max-w-[1080px] mx-auto">
     <!-- <GameTimeline :locale="store.locale" game-id="77914" /> -->
-    <mjsz-vbr-game-timeline :locale="store.locale" game-id="77928" />
+    <mjsz-vbr-game-timeline :locale="store.locale" game-id="77928" :external-player-resolver="resolver" />
   </div>
 </template>
