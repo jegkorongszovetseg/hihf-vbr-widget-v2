6bb871834b8f80a13cc29f45088aba01ef23ddae
sokamoka <akos.stegner@gmail.com>
2025-12-20 14:38:12 +0100

    fix(internal/ad-placement-tool): Link fix

--- packages/internal/src/components/ad-placement-tool/Media.vue
+++ packages/internal/src/components/ad-placement-tool/Media.vue
@@ -23,13 +23,13 @@ function setMedaiPath(path: string) {
 
 <template>
   <component :is="currentAd.link ? 'a' : 'div'" :href="currentAd.link || undefined" :target="currentAd.link ? '_blank' : undefined">
-    <video v-if="currentAd.media[0]?.mediaType?.includes('video')" :width="currentAd.media[0].width" :height="currentAd.media[0].height" autoplay muted>
-      <source :src="currentAd.media[0]?.media" :type="currentAd.media[0]?.mediaType">
+    <video v-if="currentAd.media?.[0]?.mediaType?.includes('video')" :width="currentAd.media?.[0].width" :height="currentAd.media?.[0].height" autoplay muted>
+      <source :src="currentAd.media?.[0]?.media" :type="currentAd.media?.[0]?.mediaType">
     </video>
     <picture v-else>
       <source v-if="currentAd.type === 'responsive'" :srcset="setMedaiPath(currentAd.media[1].path)" :media="mediaQuery">
-      <img v-if="currentAd.type === 'popover'" :src="setMedaiPath(currentAd.media[0].path)" :style="`width: clamp(0px, ${currentAd.media[0].width}px, 750px); height: auto;`">
-      <img v-else :src="setMedaiPath(currentAd.media[0].path)">
+      <img v-if="currentAd.type === 'popover'" :src="setMedaiPath(currentAd.media?.[0].path)" :style="`width: clamp(0px, ${currentAd.media?.[0].width}px, 750px); height: auto;`">
+      <img v-else :src="setMedaiPath(currentAd.media?.[0].path)">
     </picture>
   </component>
 </template>
